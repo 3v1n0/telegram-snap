5b71a060e07ba0e213b7a62869b8d7abd8f314be
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-12-19 14:19:37 +0100

    qtbuilder: add g++ as build packages

--- parts/plugins/x-qtbuilder.py
+++ parts/plugins/x-qtbuilder.py
@@ -67,7 +67,7 @@ class QtBuilderPlugin(make.MakePlugin):
 
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
-        self.build_packages.extend(['patch', 'perl', 'wget'])
+        self.build_packages.extend(['g++', 'patch', 'perl', 'wget'])
         self.options.source_branch = self.options.qt_version
 
     def pull(self):
